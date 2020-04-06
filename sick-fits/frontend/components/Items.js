be8b93756638ad83ffa6b18a926893c6629d181c
Frank Congson <fcongson@gmail.com>
2020-04-06 15:06:28 +1200

    Add pagination to items

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -2,11 +2,13 @@ import gql from "graphql-tag";
 import React, { Component } from "react";
 import { Query } from "react-apollo";
 import styled from "styled-components";
+import { perPage } from "../config";
 import Item from "./Item";
+import Pagination from "./Pagination";
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -33,7 +35,14 @@ class Items extends Component {
   render() {
     return (
       <Center>
-        <Query query={ALL_ITEMS_QUERY}>
+        <Pagination page={this.props.page} />
+        <Query
+          query={ALL_ITEMS_QUERY}
+          // fetchPolicy="network-only"
+          variables={{
+            skip: this.props.page * perPage - perPage
+          }}
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Errors: {error.message}</p>;
@@ -46,6 +55,7 @@ class Items extends Component {
             );
           }}
         </Query>
+        <Pagination page={this.props.page} />
       </Center>
     );
   }
