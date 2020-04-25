d0c64e813dd4271fc2b093f29aed8a468aa8e0a8
Frank Congson <fcongson@gmail.com>
2020-04-25 22:04:09 +1200

    Add permissions management page

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -19,14 +19,27 @@ server.express.use((req, res, next) => {
   next();
 });
 
+// 2. Create a middleware that populates the user on each request
+
+server.express.use(async (req, res, next) => {
+  // if they aren't logged in, skip this
+  if (!req.userId) return next();
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    "{ id, permissions, email, name }"
+  );
+  req.user = user;
+  next();
+});
+
 server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL
-    }
+      origin: process.env.FRONTEND_URL,
+    },
   },
-  deets => {
+  (deets) => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
