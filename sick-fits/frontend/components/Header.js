a5bce9dd9937b14eb4ba0fa27dc107c90d758438
Frank Congson <fcongson@gmail.com>
2020-05-05 15:30:56 +1200

    Add cart components

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Router from "next/router";
 import NProgress from "nprogress";
 import styled from "styled-components";
+import Cart from "./Cart";
 import Nav from "./Nav";
 
 Router.onRouteChangeStart = () => {
@@ -22,7 +23,7 @@ const Logo = styled.h1`
   transform: skew(-7deg);
   a {
     padding: 0.5rem 1rem;
-    background: ${props => props.theme.red};
+    background: ${(props) => props.theme.red};
     color: white;
     text-transform: uppercase;
     text-decoration: none;
@@ -35,7 +36,7 @@ const Logo = styled.h1`
 
 const StyledHeader = styled.header`
   .bar {
-    border-bottom: 10px solid ${props => props.theme.black};
+    border-bottom: 10px solid ${(props) => props.theme.black};
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
@@ -48,7 +49,7 @@ const StyledHeader = styled.header`
   .sub-bar {
     display: grid;
     grid-template-columns: 1fr auto;
-    border-bottom: 1px solid ${props => props.theme.lightgrey};
+    border-bottom: 1px solid ${(props) => props.theme.lightgrey};
   }
 `;
 
@@ -65,7 +66,7 @@ const Header = () => (
     <div className="sub-bar">
       <p>Search</p>
     </div>
-    <div>Cart</div>
+    <Cart />
   </StyledHeader>
 );
 
