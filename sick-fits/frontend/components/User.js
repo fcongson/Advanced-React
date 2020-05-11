62d93870a4df64e2d1ab7263f39296c22456e574
Frank Congson <fcongson@gmail.com>
2020-05-11 15:50:32 +1200

    Add cart item details

--- sick-fits/frontend/components/User.js
+++ sick-fits/frontend/components/User.js
@@ -9,18 +9,29 @@ const CURRENT_USER_QUERY = gql`
       email
       name
       permissions
+      cart {
+        id
+        quantity
+        item {
+          id
+          price
+          image
+          title
+          description
+        }
+      }
     }
   }
 `;
 
-const User = props => (
+const User = (props) => (
   <Query {...props} query={CURRENT_USER_QUERY}>
-    {payload => props.children(payload)}
+    {(payload) => props.children(payload)}
   </Query>
 );
 
 User.propTypes = {
-  children: PropTypes.func.isRequired
+  children: PropTypes.func.isRequired,
 };
 
 export default User;
