e4d32fc79cfc6b1a2609b99ddb62d2326d489367
Frank Congson <fcongson@gmail.com>
2020-05-25 16:12:24 +1200

    Fix button font

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -10,16 +10,16 @@ const theme = {
   lightgrey: "#e1e1e1",
   offWhite: "#ededed",
   maxWidth: "1000px",
-  bs: "0 12px 24px 0 rgba(0,0,0,0.09)"
+  bs: "0 12px 24px 0 rgba(0,0,0,0.09)",
 };
 
 const StyledPage = styled.div`
   background: white;
-  color: ${props => props.theme.black};
+  color: ${(props) => props.theme.black};
 `;
 
 const Inner = styled.div`
-  max-width: ${props => props.theme.maxWidth};
+  max-width: ${(props) => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
 `;
@@ -49,6 +49,7 @@ injectGlobal`
     text-decoration: none;
     color: ${theme.black};
   }
+  button {  font-family: 'radnika_next'; }
 `;
 
 class Page extends Component {
